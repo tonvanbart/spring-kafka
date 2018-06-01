fbdd7a2ae9da3dce0c6f0e9257f4a2f317bd42e0
Ton van Bart <ton.vanbart@kpn.com>
2018-06-01 11:17:47 +0200

    consumer can return flux of strings, controller returns SSE

--- src/main/java/com/kpn/springkafka/consumer/MessageConsumer.java
+++ src/main/java/com/kpn/springkafka/consumer/MessageConsumer.java
@@ -5,9 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MessageConsumer {
+
+    private final List<FluxSink<String>> clients = new ArrayList<>();
 
     private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
 
@@ -15,9 +22,34 @@ public class MessageConsumer {
         log.info("Initialized - Consumer");
     }
 
+    /**
+     * Receive a message and forward it to all registered clients.
+     * @param consumerRecord
+     */
     @KafkaListener(topics = {"producerTopic"}, groupId = "foo")
-    public void receiveMessage(ConsumerRecord<?,?> consumerRecord) {
+    public void receiveMessage(ConsumerRecord<?, ?> consumerRecord) {
         log.debug("receiveMessage({})", consumerRecord.value());
+        clients.forEach(client -> {
+            String msg = consumerRecord.value().toString();
+            log.debug("forward {} to {}", msg, client);
+            client.next(msg);
+        });
+    }
+
+    /**
+     * Register a new client and returns a Flux of messages.
+     * @return
+     */
+    public Flux<String> followMessages() {
+        log.info("followMessages()");
+        return Flux.create(sink -> {
+            log.info("adding client {}", sink);
+            clients.add(sink);
+            sink.onCancel(() -> {
+                log.debug("cancel {}", sink);
+                clients.remove(sink);
+            });
+        });
     }
 
 }
--- src/main/java/com/kpn/springkafka/controllers/SseController.java
+++ src/main/java/com/kpn/springkafka/controllers/SseController.java
@@ -1,7 +1,9 @@
 package com.kpn.springkafka.controllers;
 
+import com.kpn.springkafka.consumer.MessageConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,25 @@ import reactor.core.publisher.Flux;
 @RestController
 public class SseController {
 
+    private final MessageConsumer messageConsumer;
+
     private static final Logger log = LoggerFactory.getLogger(SseController.class);
+
+    @Autowired
+    public SseController(MessageConsumer messageConsumer) {
+        this.messageConsumer = messageConsumer;
+        log.info("initialized: {}", messageConsumer);
+    }
 
     @GetMapping(path="/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> messages() {
         log.info("messages()");
+        return messageConsumer.followMessages();
+    }
+
+    @GetMapping(path="/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> test() {
+        log.info("test()");
         return Flux.just("To Do");
     }
 
